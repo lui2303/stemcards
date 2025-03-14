8d2394791918faf7071cd718060a43ca73f8a2b0
lui2303 <wiederhold.luis@gmx.de>
2025-03-14 15:29:44 +0100

    Added password encoding with Bcrypt

--- backend/src/main/java/com/luiswiederhold/backend/BackendApplication.java
+++ backend/src/main/java/com/luiswiederhold/backend/BackendApplication.java
@@ -2,6 +2,9 @@ package com.luiswiederhold.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,9 +16,13 @@ public class BackendApplication {
         return "Hello World!";
     }
 
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
-
 
     }
 
