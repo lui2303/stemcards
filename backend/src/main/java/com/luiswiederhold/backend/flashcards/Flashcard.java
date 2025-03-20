e4ca70e1f755c12651644e16d4d81af350a015b3
lui2303 <wiederhold.luis@gmx.de>
2025-03-20 23:52:01 +0100

    created basic flashcard model  inside Flashcard.java, might change in the future

---
+++ backend/src/main/java/com/luiswiederhold/backend/flashcards/Flashcard.java
@@ -0,0 +1,18 @@
+package com.luiswiederhold.backend.flashcards;
+
+public interface Flashcard {
+    // maybe abstract class
+
+    /*
+    Schema:
+
+    {
+        "is_url_to_ressource": false,
+        "value" : "Latex output oder link zu der Datei, die handschrifltich geschriebenes speichert. Ggf. muss dieses image für den browser rescaled werden" TODO: endpoint /rescale implementieren oder direkt im frontend vornehmen
+        "creation_date": 23.03.2005,
+        "last_updated_on":23.03.2008,
+        "last_revised_on": 23.03.2009,
+    }
+     */
+
+}
