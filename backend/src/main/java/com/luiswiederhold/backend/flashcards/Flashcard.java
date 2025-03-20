ef7e1c8c1e2a4f1f35e5e861bb657c2b56958b8d
lui2303 <wiederhold.luis@gmx.de>
2025-03-20 23:57:43 +0100

    made Flashcard.java abstract

--- backend/src/main/java/com/luiswiederhold/backend/flashcards/Flashcard.java
+++ backend/src/main/java/com/luiswiederhold/backend/flashcards/Flashcard.java
@@ -1,6 +1,8 @@
 package com.luiswiederhold.backend.flashcards;
 
-public interface Flashcard {
+import java.util.Date;
+
+public abstract class Flashcard {
     // maybe abstract class
 
     /*
@@ -14,5 +16,57 @@ public interface Flashcard {
         "last_revised_on": 23.03.2009,
     }
      */
+     private boolean isUrlToResource;
+     private String value;
+     private Date creationDate;
+     private Date lastUpdatedOn;
+     private Date lastRevisedOn;
 
+    public Flashcard(boolean isUrlToResource, String value, Date creationDate, Date lastUpdatedOn, Date lastRevisedOn) {
+        this.isUrlToResource = isUrlToResource;
+        this.value = value;
+        this.creationDate = creationDate;
+        this.lastUpdatedOn = lastUpdatedOn;
+        this.lastRevisedOn = lastRevisedOn;
+    }
+
+    public boolean isUrlToResource() {
+        return isUrlToResource;
+    }
+
+    public void setUrlToResource(boolean urlToResource) {
+        isUrlToResource = urlToResource;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Date getLastUpdatedOn() {
+        return lastUpdatedOn;
+    }
+
+    public void setLastUpdatedOn(Date lastUpdatedOn) {
+        this.lastUpdatedOn = lastUpdatedOn;
+    }
+
+    public Date getLastRevisedOn() {
+        return lastRevisedOn;
+    }
+
+    public void setLastRevisedOn(Date lastRevisedOn) {
+        this.lastRevisedOn = lastRevisedOn;
+    }
 }
