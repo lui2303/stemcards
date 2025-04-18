c64f15a398ec0dfdc821494f7560fffb69541cb1
lui2303 <wiederhold.luis@gmx.de>
2025-04-18 17:32:43 +0200

    implemented an eraser with small medium and large mode plus a button to turn it off

--- frontend/dist/create_flashcard.js
+++ frontend/dist/create_flashcard.js
@@ -5,9 +5,17 @@ var CanvasMode;
     CanvasMode[CanvasMode["pencil"] = 1] = "pencil";
     CanvasMode[CanvasMode["image"] = 2] = "image";
 })(CanvasMode || (CanvasMode = {}));
+var EraserSize;
+(function (EraserSize) {
+    EraserSize[EraserSize["small"] = 0] = "small";
+    EraserSize[EraserSize["medium"] = 1] = "medium";
+    EraserSize[EraserSize["large"] = 2] = "large";
+    EraserSize[EraserSize["none"] = 3] = "none";
+})(EraserSize || (EraserSize = {}));
 let canvasMode = CanvasMode.pencil;
 let drawing = false;
-let pencilSize = 7;
+let erasing = false;
+let eraserSize = EraserSize.none;
 let lastX = 0;
 let lastY = 0;
 const latexButton = document.getElementById("select-latex");
@@ -16,6 +24,10 @@ const drawingCanvas = document.getElementById("drawing-canvas");
 const canvasCtx = drawingCanvas.getContext("2d");
 const pencilSizeValue = document.getElementById('pencil-size-value');
 const pencilSizeInput = document.getElementById("pencil-size");
+const smallEraserButton = document.getElementById("small-eraser");
+const mediumEraserButton = document.getElementById("medium-eraser");
+const largeEraserButton = document.getElementById("large-eraser");
+const stopEraserButton = document.getElementById('draw');
 const ratio = window.devicePixelRatio || 1;
 drawingCanvas.width = drawingCanvas.offsetWidth * ratio;
 drawingCanvas.height = drawingCanvas.offsetHeight * ratio;
@@ -38,11 +50,23 @@ function drawSmoothLine(x, y) {
     if (!canvasCtx)
         return;
     canvasCtx.beginPath();
-    canvasCtx.moveTo(lastX, lastY); // Move to the previous point
-    canvasCtx.lineTo(x, y); // Draw a line to the new point
-    canvasCtx.stroke(); // Apply the stroke
-    lastX = x; // Update the last position
+    canvasCtx.moveTo(lastX, lastY);
+    canvasCtx.lineTo(x, y);
+    canvasCtx.stroke();
+    lastX = x;
     lastY = y;
+}
+function erase(eraserSize, clientX, clientY) {
+    if (!canvasCtx)
+        return;
+    const rect = drawingCanvas.getBoundingClientRect();
+    const x = clientX - rect.left;
+    const y = clientY - rect.top;
+    canvasCtx.globalCompositeOperation = 'destination-out';
+    canvasCtx.beginPath();
+    canvasCtx.arc(x, y, eraserSize, 0, 2 * Math.PI);
+    canvasCtx.fill();
+    canvasCtx.globalCompositeOperation = 'source-over';
 }
 // event listener
 if (pencilSizeInput && pencilSizeValue) {
@@ -51,8 +75,6 @@ if (pencilSizeInput && pencilSizeValue) {
         pencilSizeValue.textContent = target.value;
         if (canvasCtx)
             canvasCtx.lineWidth = parseInt(target.value);
-        pencilSize = parseInt(target.value);
-        console.log(pencilSize);
     });
 }
 if (pencilButton) {
@@ -71,13 +93,35 @@ if (drawingCanvas) {
     drawingCanvas.addEventListener('mousedown', (e) => {
         if (e.button != 0)
             return;
+        if (eraserSize != EraserSize.none) {
+            erasing = true;
+            return;
+        }
         lastX = e.offsetX;
         lastY = e.offsetY;
         drawing = true;
     });
     drawingCanvas.addEventListener('mousemove', (e) => {
+        if (!canvasCtx)
+            return;
         if (drawing) {
             drawSmoothLine(e.offsetX, e.offsetY);
+        }
+        console.log(erasing);
+        console.log(eraserSize);
+        if (erasing) {
+            switch (eraserSize) {
+                case EraserSize.small:
+                    erase(20, e.clientX, e.clientY);
+                    break;
+                case EraserSize.medium:
+                    erase(40, e.clientX, e.clientY);
+                    break;
+                case EraserSize.large:
+                    erase(60, e.clientX, e.clientY);
+                    console.log("Here");
+                    break;
+            }
         }
     });
     drawingCanvas.addEventListener('mouseup', () => {
@@ -85,5 +129,26 @@ if (drawingCanvas) {
     });
     drawingCanvas.addEventListener('mouseout', () => {
         drawing = false;
+    });
+}
+if (smallEraserButton) {
+    smallEraserButton.addEventListener('click', () => {
+        eraserSize = EraserSize.small;
+    });
+}
+if (mediumEraserButton) {
+    mediumEraserButton.addEventListener('click', () => {
+        eraserSize = EraserSize.medium;
+    });
+}
+if (largeEraserButton) {
+    largeEraserButton.addEventListener('click', () => {
+        eraserSize = EraserSize.large;
+    });
+}
+if (stopEraserButton) {
+    stopEraserButton.addEventListener('click', () => {
+        erasing = false;
+        eraserSize = EraserSize.none;
     });
 }
--- frontend/src/create_flashcard.ts
+++ frontend/src/create_flashcard.ts
@@ -4,10 +4,19 @@ enum CanvasMode {
     image
 }
 
+enum EraserSize {
+    small,
+    medium,
+    large,
+    none
+}
+
 let canvasMode: CanvasMode = CanvasMode.pencil;
 let drawing: boolean = false;
 
-let pencilSize: number = 7;
+let erasing: boolean = false;
+let eraserSize: EraserSize = EraserSize.none;
+
 
 let lastX = 0;
 let lastY = 0;
@@ -15,10 +24,18 @@ let lastY = 0;
 
 const latexButton = document.getElementById("select-latex") as HTMLButtonElement;
 const pencilButton = document.getElementById("select-pencil") as HTMLButtonElement;
+
 const drawingCanvas = document.getElementById("drawing-canvas") as HTMLCanvasElement;
 const canvasCtx = drawingCanvas.getContext("2d");
+
 const pencilSizeValue = document.getElementById('pencil-size-value') as HTMLLabelElement;
 const pencilSizeInput = document.getElementById("pencil-size") as HTMLInputElement;
+
+const smallEraserButton = document.getElementById("small-eraser") as HTMLButtonElement;
+const mediumEraserButton = document.getElementById("medium-eraser") as HTMLButtonElement;
+const largeEraserButton = document.getElementById("large-eraser") as HTMLButtonElement;
+const stopEraserButton = document.getElementById('draw') as HTMLButtonElement;
+
 
 const ratio = window.devicePixelRatio || 1;
 
@@ -45,11 +62,25 @@ function drawSmoothLine(x: number, y: number) {
     if (!drawing) return;
     if(!canvasCtx) return;
     canvasCtx.beginPath();
-    canvasCtx.moveTo(lastX, lastY); // Move to the previous point
-    canvasCtx.lineTo(x, y); // Draw a line to the new point
-    canvasCtx.stroke(); // Apply the stroke
-    lastX = x; // Update the last position
+    canvasCtx.moveTo(lastX, lastY);
+    canvasCtx.lineTo(x, y);
+    canvasCtx.stroke();
+    lastX = x;
     lastY = y;
+}
+
+function erase(eraserSize: number, clientX: number, clientY: number) {
+    if(!canvasCtx) return
+
+    const rect = drawingCanvas.getBoundingClientRect();
+    const x = clientX - rect.left;
+    const y = clientY - rect.top;
+    
+    canvasCtx.globalCompositeOperation = 'destination-out';
+    canvasCtx.beginPath();
+    canvasCtx.arc(x, y, eraserSize, 0, 2 * Math.PI);
+    canvasCtx.fill();
+    canvasCtx.globalCompositeOperation = 'source-over';
 }
 
 
@@ -61,9 +92,6 @@ if (pencilSizeInput && pencilSizeValue) {
         pencilSizeValue.textContent = target.value;
 
         if(canvasCtx) canvasCtx.lineWidth = parseInt(target.value);
-        
-        pencilSize = parseInt(target.value);
-        console.log(pencilSize);
     });
   }
 
@@ -86,15 +114,42 @@ if (latexButton) {
 if(drawingCanvas) {
     drawingCanvas.addEventListener('mousedown', (e) => {
         if(e.button != 0) return;
+        if(eraserSize != EraserSize.none) {
+            erasing = true;
+            return;
+        }
+
         lastX = e.offsetX;
         lastY = e.offsetY;
         drawing = true;
         
       });
       drawingCanvas.addEventListener('mousemove', (e) => {
+        if(!canvasCtx) return;
+
         if(drawing) {
             drawSmoothLine(e.offsetX, e.offsetY)
         }
+        console.log(erasing);
+        console.log(eraserSize);
+        
+        
+        if (erasing) {
+            switch(eraserSize) {
+                case EraserSize.small:
+                    erase(20, e.clientX, e.clientY);
+                    break;
+                case EraserSize.medium:
+                    erase(40, e.clientX, e.clientY);
+                    break;
+                case EraserSize.large:
+                    erase(60, e.clientX, e.clientY);
+                    console.log("Here")
+                    break;
+            }
+
+        }
+
       });
 
 
@@ -105,5 +160,30 @@ if(drawingCanvas) {
     drawingCanvas.addEventListener('mouseout', () => {
         drawing = false;
     });
+}
+
+if(smallEraserButton) {
+    smallEraserButton.addEventListener('click', () => {
+        eraserSize = EraserSize.small;
+    });
+}
+
+if(mediumEraserButton) {
+    mediumEraserButton.addEventListener('click', () => {
+        eraserSize = EraserSize.medium;
+    });
+}
+
+if(largeEraserButton) {
+    largeEraserButton.addEventListener('click', () => {
+        eraserSize = EraserSize.large;
+    });
+}
+
+if(stopEraserButton) {
+    stopEraserButton.addEventListener('click', () => {
+        erasing = false;
+        eraserSize = EraserSize.none;
+    })
 }
 
